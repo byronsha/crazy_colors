269b394f629410728a5ee88cbf8baaea262619f2
Byron Sha <byronysha@gmail.com>
2016-12-25 19:48:03 -0800

    Add animations

--- src/components/Board.js
+++ src/components/Board.js
@@ -10,8 +10,10 @@ class Board extends React.Component {
       controlColorValue: 0
     }
 
-    this.increaseControlColorValue = this.increaseControlColorValue.bind(this)
-    this.decreaseControlColorValue = this.decreaseControlColorValue.bind(this)
+    this.increaseControlColorValue = this.increaseControlColorValue.bind(this);
+    this.decreaseControlColorValue = this.decreaseControlColorValue.bind(this);
+    this.startAnimation = this.startAnimation.bind(this);
+    this.stopAnimation = this.stopAnimation.bind(this);
   }
 
   getTile(i, j) {
@@ -19,30 +21,30 @@ class Board extends React.Component {
     const controlColorValue = this.state.controlColorValue;
 
     switch(controlColor) {
-    case 'red':
-      return {
-        red: controlColorValue,
-        green: i * 10,
-        blue: j * 10
-      }
-    case 'green':
-      return {
-        red: j * 10,
-        green: controlColorValue,
-        blue: i * 10
-      }
-    case 'blue':
-      return {
-        red: i * 10,
-        green: j * 10,
-        blue: controlColorValue
-      }
-    default:
-      return {
-        red: 0,
-        green: 0,
-        blue: 0
-      }
+      case 'red':
+        return {
+          red: controlColorValue,
+          green: i * 10,
+          blue: j * 10
+        }
+      case 'green':
+        return {
+          red: j * 10,
+          green: controlColorValue,
+          blue: i * 10
+        }
+      case 'blue':
+        return {
+          red: i * 10,
+          green: j * 10,
+          blue: controlColorValue
+        }
+      default:
+        return {
+          red: 0,
+          green: 0,
+          blue: 0
+        }
     }
   }
 
@@ -74,21 +76,54 @@ class Board extends React.Component {
     const newColorValue = this.state.controlColorValue + 25;
     if (newColorValue > 255) { return }
 
-    this.setState({ controlColorValue: newColorValue })
+    this.setState({ controlColorValue: newColorValue });
   }
 
   decreaseControlColorValue() {
     const newColorValue = this.state.controlColorValue - 25;
     if (newColorValue < 0) { return }
 
-    this.setState({ controlColorValue: newColorValue })
+    this.setState({ controlColorValue: newColorValue });
+  }
+
+  getNextColor() {
+    switch(this.state.controlColor) {
+      case 'red':
+        return 'green'
+      case 'green':
+        return 'blue'
+      case 'blue':
+        return 'red'
+      default:
+        return this.state.controlColor
+    }
+  }
+
+  startAnimation() {
+    if (this.interval) { return }
+
+    this.interval = setInterval(() => {
+      if (this.state.controlColorValue + 25 > 255) {
+        this.setState({
+          controlColor: this.getNextColor(),
+          controlColorValue: 0
+        });
+      } else {
+        this.increaseControlColorValue();
+      }
+    })
+  }
+
+  stopAnimation() {
+    clearInterval(this.interval);
+    this.interval = null;
   }
 
   render() {
     const rows = this.getRows();
 
     return (
-      <div className='container'>
+      <div className='board'>
         <div>
           {rows.map((row, idx) => {
             return <Row key={idx} row={row} />
@@ -104,9 +139,36 @@ class Board extends React.Component {
           <button onClick={this.increaseControlColorValue}>+</button>
           <button onClick={this.decreaseControlColorValue}>-</button>
         </div>
+        <div>
+          <button onClick={this.startAnimation}>Start</button>
+          <button onClick={this.stopAnimation}>Stop</button>
+        </div>
       </div>
     )
   }
 }
 
 export default Board;
+
+// startAnimation() {
+//   if (this.interval) { return }
+//   let increasing = true;
+//
+//   this.interval = setInterval(() => {
+//     if (increasing) {
+//       if (this.state.controlColorValue + 25 > 255) {
+//         increasing = false;
+//       } else {
+//         this.increaseControlColorValue();
+//       }
+//     } else {
+//       if (this.state.controlColorValue - 25 < 0) {
+//         increasing = true;
+//         const nextColor = this.getNextColor();
+//         this.setState({ controlColor: nextColor });
+//       } else {
+//         this.decreaseControlColorValue();
+//       }
+//     }
+//   }, 50);
+// }
